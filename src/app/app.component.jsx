933cfd78fe49e4e40122d94497e4113ba75a51bd
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-08-10 16:23:23 +0200

    chore: create simple clock

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -6,7 +6,7 @@ import cls from './app.styles.css';
 
 export const App = () => (
   <main className={cls.app}>
-    <h1>clock-actors-state-machine</h1>
+    <h1 className={cls.title}>clock-actors-state-machine</h1>
     <Clock />
   </main>
 );
