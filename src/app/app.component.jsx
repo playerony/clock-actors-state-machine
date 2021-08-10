05b1fa89e062448ecd167337932d85c373ff4e79
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-08-10 16:27:33 +0200

    chore: create simple clock (#2)

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { Clock } from '../atoms';
+
 import cls from './app.styles.css';
 
-export const App = () => <h1 className={cls.title}>clock-actors-state-machine</h1>;
+export const App = () => (
+  <main className={cls.app}>
+    <h1 className={cls.title}>clock-actors-state-machine</h1>
+    <Clock />
+  </main>
+);
