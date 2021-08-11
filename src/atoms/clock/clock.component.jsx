41cb90edfb94ee12c95ceeed85dde4c7a0fb524d
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-08-11 20:56:01 +0200

    chore: create simple timer machine (#3)

--- src/atoms/clock/clock.component.jsx
+++ src/atoms/clock/clock.component.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useMachine } from '@xstate/react';
 
 import cls from './clock.styles.css';
 
-export const Clock = () => (
-  <div className={cls.clock}>
-    <div className={cls.seconds} />
-    <div className={cls['labels-vertical']} />
-    <div className={cls['labels-horizontal']} />
-  </div>
-);
+import { timerMachine } from '../../machines';
+
+export const Clock = () => {
+  const [state] = useMachine(timerMachine);
+
+  const { elapsed, duration } = state.context;
+
+  const progress = elapsed / duration;
+  const progressDegree = progress * 360;
+  const percentageProgress = progress * 100;
+  const formattedProgress = percentageProgress.toFixed(1);
+
+  return (
+    <div className={cls.clock}>
+      <div
+        className={cls.seconds}
+        style={{
+          '--degree': progressDegree,
+        }}
+      />
+      <div className={cls['labels-vertical']} />
+      <div className={cls['labels-horizontal']} />
+      <h1 className={cls.progress}>{formattedProgress}%</h1>
+    </div>
+  );
+};
