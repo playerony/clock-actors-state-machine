bbf1d09ce814a44dafc5d04d0b8dd2532a78aea1
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-08-12 16:37:49 +0200

    chore: add base layout wrapper (#5)

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -6,7 +6,11 @@ import cls from './app.styles.css';
 
 export const App = () => (
   <main className={cls.app}>
-    <Clock />
-    <Dots amountOfDots={5} activeDotIndex={1} />
+    <div className={cls['content-wrapper']}>
+      <section className={cls['clock-wrapper']}>
+        <Clock />
+        <Dots amountOfDots={5} activeDotIndex={1} />
+      </section>
+    </div>
   </main>
 );
