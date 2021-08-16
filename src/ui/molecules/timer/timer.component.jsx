a50374088501586fa9aa11ce1fbdadfc7438eaa4
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-08-16 20:00:00 +0200

    chore: add reset button for timer

--- src/ui/molecules/timer/timer.component.jsx
+++ src/ui/molecules/timer/timer.component.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { useService } from '@xstate/react';
 
-import { Clock } from '@ui';
+import { Clock, Button, HorizontalButtonsWrapper } from '@ui';
+
+import cls from './timer.styles.css';
 
 export const Timer = ({ timerRef, isActive }) => {
-  const [state] = useService(timerRef);
+  const [state, send] = useService(timerRef);
 
   const { duration, elapsed } = state.context;
 
-  return <Clock duration={duration} elapsed={elapsed} isActive={isActive} />;
+  return (
+    <div className={cls.wrapper} data-active={isActive}>
+      <Clock duration={duration} elapsed={elapsed} />
+      <HorizontalButtonsWrapper>
+        {state.matches({ running: 'normal' }) && (
+          <Button onClick={() => send('ADD_MINUTE')}>+ 1:00</Button>
+        )}
+
+        {state.matches({ running: 'normal' }) && (
+          <Button onClick={() => send('TOGGLE')}>Pause timer</Button>
+        )}
+
+        {(!state.matches({ running: 'normal' }) || state.matches({ running: 'overtime' })) && (
+          <Button onClick={() => send('RESET')}>Reset timer</Button>
+        )}
+
+        {(state.matches('paused') || state.matches('idle')) && (
+          <Button onClick={() => send('TOGGLE')}>Start timer</Button>
+        )}
+      </HorizontalButtonsWrapper>
+    </div>
+  );
 };
